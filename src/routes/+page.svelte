f2962d13bf5b181ddf685f5e940f4cfe97cbe557
sofpolette <sofpolette@gmail.com>
2023-09-26 17:40:01 +0100

    improved padding and text size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <!-- Content -->
 <div class="my-8 flex space-x-8">
 	<!-- Text -->
-	<div class="h-full space-y-16 flex-col w-1/2">
-		<p class="w-full text-4xl font-light">Hey! I’m Sofía ☻</p>
-		<p class="text-4xl font-light">I'm a Graphic Designer <br />specialized in branding.</p>
-		<p class="text-4xl font-light">
-			I’m also studying <br />Graphic Design <br />at the EASD Fernando Estévez, Canary Islands.
+	<div class="h-full justify-between flex-col w-1/2">
+		<p class="w-full text-5xl font-light">Hey! I’m Sofía ☻</p>
+		<p class="text-5xl font-light">I'm a Graphic Designer <br />specialized in branding.</p>
+		<p class="text-5xl font-light">
+			I’m also studying <br />Graphic Design <br />at the EASD Fernando <br />Estévez, Canary
+			Islands.
 		</p>
 	</div>
 	<!-- Project reel -->
