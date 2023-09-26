09e5dc8404e0d63b90315da1a8a9d18a4a67728f
sofpolette <sofpolette@gmail.com>
2023-09-26 18:35:37 +0100

    better main text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <!-- Content -->
 <div class="my-8 flex space-x-8">
 	<!-- Text -->
-	<div class="h-full justify-between flex-col w-1/2">
-		<p class="w-full text-5xl font-light">Hey! I’m Sofía ☻</p>
-		<p class="text-5xl font-light">I'm a Graphic Designer <br />specialized in branding.</p>
-		<p class="text-5xl font-light">
-			I’m also studying <br />Graphic Design <br />at the EASD Fernando <br />Estévez, Canary
-			Islands.
+	<div class="flex flex-col h-full justify-between col w-2/5">
+		<p class="text-4xl font-light">Hi there! I'm Sofía ☻</p>
+		<p class="text-4xl font-light">
+			I'm a Graphic Designer specializing in branding, editorial, multimedia and UX.
+		</p>
+		<p class="text-4xl font-light">
+			Additionally, I'm currently pursuing a Graphic Design bachelor's at EASD Fernando Estévez in
+			the Canary Islands.
 		</p>
 	</div>
 	<!-- Project reel -->
-	<img src="/index.gif" alt="Images of projects" class="col w-1/2 object-cover rounded-3xl" />
+	<img src="/index.gif" alt="Images of projects" class="col w-3/5 object-cover rounded-3xl" />
 </div>
