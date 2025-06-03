e0cff3d954532e43a509453b30ae7f5ded93027e
sofpolette <sofpolette@gmail.com>
2025-06-03 12:45:46 +0200

    added photos waia

--- src/routes/(footer)/waia/+page.svelte
+++ src/routes/(footer)/waia/+page.svelte
@@ -17,6 +17,11 @@
 </div>
 
 <div class="image-row flex justify-between w-1/3">
+	<img src="waia/photo1.JPG" alt="" class="pair-image" />
+	<img src="waia/photo2.JPG" alt="" class="pair-image right-image" />
+</div>
+
+<div class="image-row flex justify-between w-1/3">
 	<img src="waia/spread1.jpg" alt="" class="pair-image" />
 	<img src="waia/spread2.jpg" alt="" class="pair-image right-image" />
 </div>
@@ -27,6 +32,11 @@
 </div>
 
 <div class="image-row flex justify-between w-1/3">
+	<img src="waia/photo6.JPG" alt="" class="pair-image" />
+	<img src="waia/photo4.JPG" alt="" class="pair-image right-image" />
+</div>
+
+<div class="image-row flex justify-between w-1/3">
 	<img src="waia/spread5.jpg" alt="" class="pair-image" />
 	<img src="waia/spread6.jpg" alt="" class="pair-image right-image" />
 </div>
@@ -34,4 +44,10 @@
 <div class="image-row flex justify-between w-1/3">
 	<img src="waia/spread7.jpg" alt="" class="pair-image" />
 	<img src="waia/spread8.jpg" alt="" class="pair-image right-image" />
+</div>
+
+<div class="image-container">
+	<div class="full-width-image">
+		<img src="waia/photo5.JPG" alt="" />
+	</div>
 </div>
